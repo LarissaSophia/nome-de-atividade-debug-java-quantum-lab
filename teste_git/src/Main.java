cd8947ca7821760222bbf0b93b4c2cda33a4afbe
Natália Souza <nataliadesouza1707@gmail.com>
2026-04-09 14:57:22 -0300

    Novos arquivos adicionados

--- teste_git/src/Main.java
+++ teste_git/src/Main.java
@@ -9,9 +9,6 @@ public class Main {
         System.out.println("Digite seu nome: ");
         String nome = leitor.nextLine();
 
-        for(int i = 0;  i < 3; i++){
-            System.out.println("Seasdasu nome i :" + "Matheus");
-        }
         System.out.println("========== Escolha as Opções a Baixo: ==========");
         System.out.println("1 - Para Soma \n2 - Para Subtração \n3 - Para Sair");
         int opcao = leitor.nextInt();
@@ -23,7 +20,7 @@ public class Main {
                 System.out.print("Digite o segundo número: ");
                 numero2 = leitor.nextInt();
 
-                int soma = numero1 * numero2;
+                float soma = numero1 + numero2;
 
                 System.out.println("A soma de " + numero1 + " e " + numero2 + " é: " + soma);
                 break;
@@ -34,15 +31,13 @@ public class Main {
                 System.out.print("Digite o segundo número: ");
                 numero2 = leitor.nextInt();
 
-                float subtracao = numero1 / numero2;
+                float subtracao = numero1 - numero2;
 
                 System.out.println("A subtração de " + numero1 + " e " + numero2 + " é: " + subtracao);
                 break;
             case 3:
-                for(int i = 0;  i < 3; i++){
-                    System.out.println("Fiz um show SoldOut:" + "Paypal");
-                    System.out.println("saindo do Teto");
-                }
+
+                    System.out.println("saindo do sistema");
                 break;
         }
         leitor.close();
